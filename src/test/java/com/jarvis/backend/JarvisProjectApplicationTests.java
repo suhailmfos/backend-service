82adde3ac3d300401f2d91fc319427819711326e
Suhail <suhail@gmail.com>
2025-02-03 14:34:41 +0530

    sonarqube added

--- src/test/java/com/jarvis/backend/JarvisProjectApplicationTests.java
+++ src/test/java/com/jarvis/backend/JarvisProjectApplicationTests.java
@@ -1,27 +1,8 @@
 package com.jarvis.backend;
 
-import com.jarvis.backend.model.Contact;
-import com.jarvis.backend.repo.ContactRepository;
-import com.jarvis.backend.service.ContactService;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JarvisProjectApplicationTests {
-
-	@Mock
-	ContactRepository contactRepository;
-
-	@InjectMocks
-	ContactService contactService;
-
-	private Contact contact;
-
-
-	@Test
-	void contextLoads() {
-	}
 
 }
