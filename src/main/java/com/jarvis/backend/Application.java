e19f2a752aeab7bd4ac72cfd71390e76babfc355
Suhail <mohd.suhail@velocis.co.in>
2026-03-30 14:17:25 +0530

    db change

--- src/main/java/com/jarvis/backend/Application.java
+++ src/main/java/com/jarvis/backend/Application.java
@@ -1,15 +1,18 @@
 package com.jarvis.backend;
 
 import com.jarvis.backend.config.SecurityConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(SecurityConfig.class)
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
+        log.info("Application Started...");
         SpringApplication.run(Application.class, args);
     }
 }
